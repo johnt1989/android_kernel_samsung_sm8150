62c2903aff473a50d9bb03ea3b0ce2af1e082551
johnt1989 <johnjohnt1989@gmail.com>
2023-05-16 11:25:30 -0700

    test

--- build_d2q.sh
+++ build_d2q.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Variables
-DIR=`readlink -f .`;
-PARENT_DIR=`readlink -f ${DIR}/..`;
+DIR=`readlink -f .`
+OUT_DIR=$DIR/out
+PARENT_DIR=`readlink -f ${DIR}/..`
 
+CHIPSET_NAME=sm8150
+export ARCH=arm64
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
+export VARIANT=d2q
 BUILD_CROSS_COMPILE=$PARENT_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$PARENT_DIR/llvm-arm-toolchain-ship-10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="LOCALVERSION=-Doc714"
 
-export v=$(pwd)/arch/arm64/configs/doc_d2q_defconfig
-export VARIANT=d2q
+DTS_DIR=$OUT_DIR/arch/$ARCH/boot/dts
 
 toolchain()
 {
@@ -30,24 +33,24 @@ llvm()
 
 clean()
 {
-  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN clean 
-  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN mrproper
-  [ -d "out" ] && rm -rf out
+  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE clean 
+  make $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE mrproper
+  [ -d "$OUT_DIR" ] && rm -rf $OUT_DIR
 }
 
-build()
+build_kernel()
 {
-  [ ! -d "out" ] && mkdir out
-  make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN doc_d2q_defconfig
-  make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN
+  [ ! -d "$OUT_DIR" ] && mkdir $OUT_DIR
+  make -j$(nproc) -C $(pwd) O=$OUT_DIR $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE doc_d2q_defconfig
+  make -j$(nproc) -C $(pwd) O=$OUT_DIR $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
-  [ -e out/arch/arm64/boot/Image.gz ] && cp out/arch/arm64/boot/Image.gz $(pwd)/out/Image.gz
-  if [ -e out/arch/arm64/boot/Image.gz-dtb ]; then
-    cp out/arch/arm64/boot/Image.gz-dtb $(pwd)/out/Image.gz-dtb
+  [ -e $OUT_DIR/arch/arm64/boot/Image.gz ] && cp $OUT_DIR/arch/arm64/boot/Image.gz $OUT_DIR/Image.gz
+  if [ -e $OUT_DIR/arch/arm64/boot/Image.gz-dtb ]; then
+    cp $OUT_DIR/arch/arm64/boot/Image.gz-dtb $OUT_DIR/Image.gz-dtb
 
-    DTBO_FILES=$(find $(pwd)/out/arch/arm64/boot/dts/samsung/davinci/ -name sm8150-sec-d2q-overlay-r*.dtbo)
-    cat $(pwd)/out/arch/arm64/boot/dts/qcom/*.dtb > $(pwd)/out/dtb.img
-    $(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 $(pwd)/out/arch/arm64/boot/dts
+    DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-${VARIANT}-*-r*.dtbo)
+    cat ${DTS_DIR}/qcom/*.dtb > $OUT_DIR/dtb.img
+    $(pwd)/tools/mkdtimg create $OUT_DIR/dtbo.img --page_size=4096 ${DTBO_FILES}
   fi
 }
 
@@ -56,34 +59,34 @@ anykernel3()
   if [ ! -d $PARENT_DIR/AnyKernel3 ]; then
     git clone https://github.com/osm0sis/AnyKernel3 $PARENT_DIR/AnyKernel3
   fi
-  if [ -e out/arch/arm64/boot/Image.gz-dtb ]; then
-    cp out/arch/arm64/boot/Image.gz-dtb $PARENT_DIR/AnyKernel3/zImage
+  if [ -e $OUT_DIR/arch/arm64/boot/Image.gz-dtb ]; then
+    cp $OUT_DIR/arch/arm64/boot/Image.gz-dtb $PARENT_DIR/AnyKernel3/zImage
+    cp $OUT_DIR/dtb.img $PARENT_DIR/AnyKernel3/dtb.img
+    cp $OUT_DIR/dtbo.img $PARENT_DIR/AnyKernel3/dtbo.img
+    cd $PARENT_DIR/AnyKernel3
+    git reset --hard
+    cp $OUT_DIR/arch/arm64/boot/Image zImage
+    sed -i "s/ExampleKernel by osm0sis/${VARIANT} kernel by Doc714/g" anykernel.sh
+    sed -i "s/do\.devicecheck=1/do\.devicecheck=0/g" anykernel.sh
+    sed -i "s/=maguro/=/g" anykernel.sh
+    sed -i "s/=toroplus/=/g" anykernel.sh
+    sed -i "s/=toro/=/g" anykernel.sh
+    sed -i "s/=tuna/=/g" anykernel.sh
+    sed -i "s/platform\/omap\/omap_hsmmc\.0\/by-name\/boot/bootdevice\/by-name\/boot/g" anykernel.sh
+    sed -i "s/backup_file/#backup_file/g" anykernel.sh
+    sed -i "s/replace_string/#replace_string/g" anykernel.sh
+    sed -i "s/insert_line/#insert_line/g" anykernel.sh
+    sed -i "s/append_file/#append_file/g" anykernel.sh
+    sed -i "s/patch_fstab/#patch_fstab/g" anykernel.sh
+    sed -i "s/dump_boot/split_boot/g" anykernel.sh
+    sed -i "s/write_boot/flash_boot/g" anykernel.sh
+    zip -r9 $PARENT_DIR/d2q_kernel.zip * -x .git README.md *placeholder
+    cd $DIR
   fi
-  if [ -e $(pwd)out/dtb.img ]; then
-    cp $(pwd)/out/dtb.img $PARENT_DIR/AnyKernel3/dtb.img
-  fi
-  if [ -e $(pwd)out/dtbo.img ]; then
-    cp $(pwd)/out/dtbo.img $PARENT_DIR/AnyKernel3/dtbo.img
-  fi
-  cd $PARENT_DIR/AnyKernel3
-  git reset --hard
-  sed -i "s/ExampleKernel by osm0sis/d2q kernel by Doc714/g" anykernel.sh
-  sed -i "s/=maguro/=d2q/g" anykernel.sh
-  sed -i "s/=toroplus/=/g" anykernel.sh
-  sed -i "s/=toro/=/g" anykernel.sh
-  sed -i "s/=tuna/=/g" anykernel.sh
-  sed -i "s/omap\/omap_hsmmc\.0\/by-name\/boot/soc\/1d84000\.ufshc\/by-name\/boot/g" anykernel.sh
-  sed -i "s/backup_file/#backup_file/g" anykernel.sh
-  sed -i "s/replace_string/#replace_string/g" anykernel.sh
-  sed -i "s/insert_line/#insert_line/g" anykernel.sh
-  sed -i "s/append_file/#append_file/g" anykernel.sh
-  sed -i "s/patch_fstab/#patch_fstab/g" anykernel.sh
-  zip -r9 $PARENT_DIR/d2q_kernel.zip * -x .git README.md *placeholder
-  cd $DIR
 }
 
 toolchain
 llvm
 clean
-build
+build_kernel
 anykernel3
